4e947cb6bde62d98087b7b18defd6052adbed92c
Hyunwoo Lee <lhs739300@gmail.com>
2026-06-19 17:23:38 +0900

    ADD | 괄호 및 Stold 예외처리 추가

--- main.cpp
+++ main.cpp
@@ -8,8 +8,19 @@
 using namespace std;
 
 #pragma region Utils
+long double ExStold(string a){
+    long double temp = 0;
+    try{
+        temp = stold(a);
+    }
+    catch(exception e){
+        return numeric_limits<long double>::quiet_NaN();
+    }
+    return temp;
+}
+
 bool IsOp(char a){
-    return (a == '+' || a == '-' || a == '*' || a == '/');
+    return (a == '+' || a == '-' || a == '*' || a == '/' || a == '^' || a == 'v');
 }
 
 bool IsMinus(char a){
@@ -34,7 +45,7 @@ long double Division(long double a, long double b){
 
 long double SqrtLong(long double a, long double b){
     if(b == 0) return a;
-    int temp = a;
+    long double temp = a;
     for(int i = 0; i < b; i++){
         temp = sqrt(temp);
     }
@@ -45,8 +56,8 @@ long double SqrtLong(long double a, long double b){
 int findFirstIndex(int a, int b){
     if(b == -1) return a;
     else if(a == -1) return b;
-    else if(a > b) return a;
-    else if(a < b) return b;
+    else if(a < b) return a;
+    else if(a > b) return b;
     else return -1;
 }
 
@@ -65,15 +76,15 @@ string findLNumber(string str, int middleIndex){
             break;
     }
     if (cnt == 0)
-        return 0;
+        return "";
     else
-        return isMinus ? "-" : "" + str.substr(str.length()-cnt);
+        return (isMinus ? "-" : "") + str.substr(middleIndex - cnt, cnt);
 }
 
 string findRNumber(string str, int middleIndex){
     int cnt = 0;
     bool isMinus = false;
-    for (int i = middleIndex - 1; i < str.length(); i++) {
+    for (int i = middleIndex + 1; i < str.length(); i++) {
         if (!IsOp(str[i])) {
             cnt++;
         }
@@ -85,9 +96,9 @@ string findRNumber(string str, int middleIndex){
             break;
     }
     if (cnt == 0)
-        return 0;
+        return "";
     else
-        return isMinus ? "-" : "" + str.substr(str.length()-cnt);
+        return (isMinus ? "-" : "") + str.substr(middleIndex + 1, cnt);
 }
 #pragma endregion
 
@@ -118,37 +129,87 @@ long double simpleCal(char op, long double a, long double b){
 long double Calculate(string& input)
 {
     while(1){
+        int Lindex = -1;
+        int Rindex = -1;
+        for(int i = 0; i < input.length(); i++){
+            if(input[i] == '('){
+                Lindex = i;
+            }
+            else if(input[i] == ')'){
+                Rindex = i;
+                string baseFormula = input.substr(Lindex + 1, Rindex - Lindex - 1);
+                cout << "Fomula Cal : " << baseFormula << "\n";
+                long double result = Calculate(baseFormula);
+                input = input.substr(0, Lindex) + to_string(result) + input.substr(Rindex + 1);
+            }
+        }
+
+        if(Lindex == -1 && Rindex == -1){
+            break;
+        }
+        else if(Lindex == -1 || Rindex == -1){
+            return numeric_limits<long double>::quiet_NaN();
+        }
+    }
+
+    while(1){
+        int index = findFirstIndex(input.find('^'), input.find('v'));
+
+        if(index > -1){
+            string LNumberStr = findLNumber(input, index);
+            string RNumberStr = findRNumber(input, index);
+
+            cout << LNumberStr << " " << input[index] << " " << RNumberStr << "\n"; 
+
+            long double LNumber = ExStold(LNumberStr);
+            long double RNumber = ExStold(RNumberStr);
+            
+            long double result = simpleCal(input[index], LNumber, RNumber);
+            input = input.substr(0, index - LNumberStr.length()) + to_string(result) + input.substr(index + 1, RNumberStr.length());
+        }
+        
+        if(index == -1) break;
+    }
+
+    while(1){
         int index = findFirstIndex(input.find('*'), input.find('/'));
 
         if(index > -1){
             string LNumberStr = findLNumber(input, index);
             string RNumberStr = findRNumber(input, index);
 
-            long double LNumber = stold(LNumberStr);
-            long double RNumber = stold(RNumberStr);
+            cout << LNumberStr << " " << input[index] << " " << RNumberStr << "\n"; 
+
+            long double LNumber = ExStold(LNumberStr);
+            long double RNumber = ExStold(RNumberStr);
             
             long double result = simpleCal(input[index], LNumber, RNumber);
-            //return input.substr(index - LNumberStr.length, )
+            input = input.substr(0, index - LNumberStr.length()) + to_string(result) + input.substr(index + 1, RNumberStr.length());
         }
+        
+        if(index == -1) break;
+    }
+    while(1){
 
-        index = findFirstIndex(input.find('+'), input.find('-'));
+        int index = findFirstIndex(input.find('+'), input.find('-'));
         
         if(index > -1){
             string LNumberStr = findLNumber(input, index);
             string RNumberStr = findRNumber(input, index);
 
-            long double LNumber = stold(LNumberStr);
-            long double RNumber = stold(RNumberStr);
+            cout << LNumberStr << " " << input[index] << " " << RNumberStr << "\n"; 
+
+            long double LNumber = ExStold(LNumberStr);
+            long double RNumber = ExStold(RNumberStr);
             
             long double result = simpleCal(input[index], LNumber, RNumber);
+            input = input.substr(0, index - LNumberStr.length()) + to_string(result) + input.substr(index + 1 + RNumberStr.length());
         }
 
-        if(index == -1){
-            break;
-        }
+        if(index == -1) break;
     }
 
-    return stold(input);
+    return ExStold(input);
 }
 #pragma endregion
 
@@ -162,6 +223,7 @@ int main(){
     cout << "입력된 수식: " << input << endl;
     long double result = Calculate(input);
     if(isnan(result)) cout << "잘못된 수식입니다!";
+    else cout << "답 : " << result << "\n";
 
     return 0;
 }
